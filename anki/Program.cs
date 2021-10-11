f3dd977e45171d9664fe7facabf6973cef45d80c
NitroLine <48989608+NitroLine@users.noreply.github.com>
2021-10-11 21:56:54 +0500

    Add project

---
+++ anki/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
