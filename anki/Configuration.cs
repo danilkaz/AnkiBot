7c87c5ab31b87621f12cfb2b36f84153344c6667
NitroLine <sergey.and.natasha@yandex.ru>
2021-10-14 16:50:04 +0500

    Delete Configuration.cs

--- anki/Configuration.cs
+++
@@ -1,7 +0,0 @@
-namespace Telegram.Bot.Examples.Echo
-{
-    public static class Configuration
-    {
-        public readonly static string BotToken = "1316925286:AAEX9pd8qFrTnuKgT2rcOdNyC3AFYDxrjYI";
-    }
-}
