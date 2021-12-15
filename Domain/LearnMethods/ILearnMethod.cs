979b78ff2c9626427694b08a9a692327d90a75b7
AlexMoshkov <9aq360zqmuf1@mail.ru>
2021-12-15 18:12:07 +0500

    add description to learn methods

--- Domain/LearnMethods/ILearnMethod.cs
+++ Domain/LearnMethods/ILearnMethod.cs
@@ -6,6 +6,9 @@ namespace AnkiBot.Domain.LearnMethods
     public interface ILearnMethod
     {
         string Name { get; }
+
+        string Description { get; }
+
         TimeSpan GetNextRepetition(Card card, int answer);
 
         IParameters GetParameters();
